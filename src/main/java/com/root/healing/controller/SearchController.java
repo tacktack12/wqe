1d3ca9aef2b58bdad553539ed3b96780c1d2e2ce
kitcoop <kitcoop@kitcoop-PC>
2017-10-17 20:31:21 +0900

    123

--- src/main/java/com/root/healing/controller/SearchController.java
+++ src/main/java/com/root/healing/controller/SearchController.java
@@ -16,6 +16,7 @@ public class SearchController {
 	
 	@Autowired
 	private SawonDao dao;
+	// 수정된 서치 컨트롤러
 	
 	@RequestMapping(value="search", method=RequestMethod.POST)
 	public ModelAndView searchProcess(SangBean bean) {
